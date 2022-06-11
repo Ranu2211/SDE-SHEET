93d3e38c34f203c9b0a73c09fb2c7bcf0fe27c3f
ranu2211 <ranusinghal782@gmail.com>
2022-06-12 00:02:53 +0530

    Done

---
+++ Day 7/remove_duplicate.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int removeDuplicates(vector<int>& nums) {
+        if(nums.size() < 2) 
+            return nums.size();
+        int id = 1;
+        for(int i = 1; i < nums.size(); ++i){ 
+            if(nums[i] != nums[i-1])
+                nums[id++] = nums[i];
+        }
+        return id;
+    }
+int main(){
+    int n;
+    cin>>n;
+   vector<int>nums(n);
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    cout<<removeDuplicates(nums);
+    return 0;
+}
